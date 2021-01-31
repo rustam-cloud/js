bb2d02482297fd7bb3ffb72d9032875ea87a27c3
Rustam <gasymov86@gmail.com>
2021-01-31 19:33:10 +0200

    First

---
+++ Script.js
@@ -0,0 +1,59 @@
+"use strict";
+
+//let res = 5 % 2;
+
+let a = 5;
+
+//let res = eval('8 +5');
+
+//let data =  parseInt('105343.84');
+//let data =  parseInt(prompt('Введи число'));
+
+//let res =  (5 == 5) && (4 != 3 ) ? 'Правильно' : 'Не правильно';
+
+
+let num1 = 5;
+
+let num2 = 15;
+
+//num1 += num2;
+
+
+// if (num1 == num2){
+//     alert('Верно');
+// }else if(num1 >= num2){
+//     alert('Число больше');
+// }else {
+//     alert('Число меньше');
+// }
+
+
+let myNum = 20;
+
+switch (myNum) {
+
+    case 6:
+    alert('num 6');
+    break;
+
+    case 5:
+    alert('num 5');
+   // myNum = 10;
+    break;
+
+    case 10:
+    alert('num 10');
+    break;
+
+    default:
+    alert('not number');
+
+}
+
+
+
+
+//console.log(typeof data);
+//document.write(res);
+
+//Задача1
